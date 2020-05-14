c8cead130c9d6a36a2546ebe7414cb4783218068
Yerba <50071295+YerbaPage@users.noreply.github.com>
2020-05-14 14:30:50 +0800

    Update easy_simplex.py

--- easy_simplex.py
+++ easy_simplex.py
@@ -17,9 +17,18 @@ def printSol():
 d = np.loadtxt("data.txt", dtype=np.float)
 (bn,cn) = d.shape
 s = list(range(cn-bn,cn-1)) #基变量列表
-solve()
+loop = 0
+while max(d[-1][:-1]) > 0 and loop<100:
+    jnum = np.argmax(d[-1][:-1])  # 转入下标
+    inum = np.argmin(d[:-1, -1]/d[:-1, jnum])  # 转出下标
+    s[inum] = jnum  # 更新基变量
+    d[inum] /= d[inum][jnum]
+    for i in range(bn):
+         if i != inum:
+            d[i] -= d[i][jnum] * d[inum]
+    loop += 1
+    print(loop, "objective is %.2f" % (-d[-1][-1]))
 printSol()
-
 #https://blog.csdn.net/kittyzc/article/details/81707464
 # data.txt: 
 # 1 1 1 1 0 0 0 4
